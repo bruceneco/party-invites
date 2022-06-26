b84f4dce03412402f6a293f57e47307d354f2f5f
Bruce Neco <bruce.neco3@outlook.com>
2022-06-25 21:44:51 -0300

    refactor: separate main steps in funcs

--- main.go
+++ main.go
@@ -2,32 +2,48 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/bruceneco/party-invites/utils"
 	"github.com/gorilla/mux"
 )
 
+var cfg *utils.Config
+var sm http.Handler
+
 func main() {
-	l := log.Logger{}
-	utils.InitLogger(&l)
-	cfg, err := utils.LoadConfig()
+	stepLog()
+	stepEnv()
+	stepServer()
+	stepListenAndServe()
+}
+
+func stepLog() {
+	utils.InitLogger()
+}
+
+func stepEnv() {
+	var err error
+	cfg, err = utils.LoadConfig()
 	if err != nil {
-		l.Fatalln(err)
+		utils.ErrorLog.Fatalln(err)
 	}
+}
 
-	host := cfg.Server.Host
-	port := cfg.Server.Port
-
+func stepServer() {
 	sm := mux.NewRouter()
 
 	sm.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, Gopher!"))
 	})
-	log.Printf("[INFO] Server started at %s:%s\n", host, port)
-	err = http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), sm)
+}
+
+func stepListenAndServe() {
+	host := cfg.Server.Host
+	port := cfg.Server.Port
+	utils.InfoLog.Printf("Server started at %s:%s\n", host, port)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), sm)
 	if err != nil {
-		l.Fatalln(err)
+		utils.ErrorLog.Fatalln(err)
 	}
 }
