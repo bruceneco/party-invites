9f4bc35cc15417e4c6599dded7768150b37a8c9d
Bruce Neco <bruce.neco3@outlook.com>
2022-06-28 22:56:46 -0300

    feat: add nullable int to invite id

--- datastruct/guest.go
+++ datastruct/guest.go
@@ -1,9 +1,11 @@
 package datastruct
 
+import "database/sql"
+
 const GuestTableName = "guests"
 
 type Guest struct {
-	ID       int    `db:"id"`
-	Name     string `db:"name"`
-	InviteID int    `db:"invite_id"`
+	ID       int           `db:"id"`
+	Name     string        `db:"name"`
+	InviteID sql.NullInt64 `db:"invite_id"`
 }
