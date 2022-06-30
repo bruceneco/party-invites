5827a38989a8b3f79fdf51fe20c30c292a613531
Bruce Neco <bruce.neco3@outlook.com>
2022-06-30 09:31:23 -0300

    doc: add project info to swagger

--- handler/handler.go
+++ handler/handler.go
@@ -1,3 +1,19 @@
+// Package handlers Party Invites API.
+//
+// This application will manage invites for a party.
+//
+//     Schemes: http, https
+//     BasePath: /
+//     Version: 0.0.1
+//     Contact: Bruce Neco<bruce.neco3@outlook.com>
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
 package handler
 
 import "github.com/gorilla/mux"
